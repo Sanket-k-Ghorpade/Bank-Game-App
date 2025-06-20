0325c13e05f228186522d502f861c4999b49e9d9
sanket..ghorpade <sanket..ghorpade@HP2335>
2025-06-20 19:29:44 +0530

    Add project files.

---
+++ Bank Game App/BankAccount.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_Game_App
+{
+    public class BankAccount
+    {
+        private string accountholder;
+        private double balance;
+
+        // Getters and Setters
+        public string Accountholder {  get { return accountholder; } set { accountholder = value; }}
+        public double Balance { get { return balance; } set { balance = value; }}
+
+
+        // Constructor
+        public BankAccount(string accountholder, double balance)
+        {
+            this.accountholder = accountholder;
+            this.balance = balance;
+        }
+
+        // Deposit method
+        private void Deposit(double amount)
+        {
+            balance += amount;
+            balance = Math.Round(balance, 2);
+        }
+
+        // public method DepositCaller() to access private method Deposit() -> following encapsulation principles
+        public void DepositCaller()
+        {
+            // Input amount validation to check if its in the range 1 - 1000, if not then prompts the user to enter it repeatadly until valid amount is entered
+            double amount;
+            do
+            {
+                Console.Write("Amount to deposit: ");
+                amount = double.Parse(Console.ReadLine());
+                if (amount < 0 || amount > 1000) Console.WriteLine("Invalid amount! Please enter again");
+            }
+            while (amount < 0 || amount > 1000);
+
+            Deposit(amount);
+        }
+
+        // Withdraw method
+        private void Withdraw(double amount)
+        { 
+            balance -= amount; 
+            balance = Math.Round(balance,2);
+        }
+
+        // public method WithdrawCaller() to access private method Withdraw() -> following encapsulation principles
+        public void WithdrawCaller()
+        {
+            // Input amount validation to check if its in the range 1 - 1000, if not then prompts the user to enter it repeatadly until valid amount is entered
+            double amount;
+            do
+            {
+                Console.Write("Amount to Withdraw: ");
+                amount = double.Parse(Console.ReadLine());
+                if (amount < 0 || amount > 1000) Console.WriteLine("Invalid amount! Please enter again");
+            }
+            while (amount < 0 || amount > 1000);
+            Withdraw(amount);
+        }
+    }
+}
---
+++ Bank Game App/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Bank_Game_App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("----------------- BANK GAME APP ------------------");
+            Console.WriteLine("\n** Rules of the game **");
+            Console.WriteLine("-> Both players will be assigned a RANDOM Bank balance in the range 1000 - 5000");
+            Console.WriteLine("-> There will be 3 rounds, in each round a player may withdraw or deposit certain amount in his balance");
+            Console.WriteLine("-> Note that this amount can be in the range 1 - 1000 only");
+            Console.WriteLine("-> The player with high bank balance after 3 rounds will win");
+
+            double GetRandomNumber(double min,double max)
+            {
+                Random rnd = new Random();
+                return rnd.NextDouble() * (max - min) + min;
+            }
+            BankAccount a1 = new BankAccount("Account1", GetRandomNumber(1000,5000));
+            BankAccount a2 = new BankAccount("Account2", GetRandomNumber(1000,5000));
+
+            int round = 1;
+
+            while (round <= 3)
+            {
+                Console.WriteLine($"\nRound {round}:");
+
+                string choice1, choice2;
+
+                // Input choice validation
+                do
+                {
+                    Console.Write($"{a1.Accountholder} Withdraw or Deposit (w/d): ");
+                    choice1 = Console.ReadLine();
+                    if (choice1 == "w") a1.WithdrawCaller();
+                    else if (choice1 == "d") a1.DepositCaller();
+                    else Console.WriteLine("Invalid Choice! Please enter again");
+
+                }
+                while (choice1 != "w" && choice1 != "d");
+
+                // Input choice validation
+                do
+                {
+                    Console.Write($"{a2.Accountholder} Withdraw or Deposit (w/d): ");
+                    choice2 = Console.ReadLine();
+                    if (choice2 == "w") a2.WithdrawCaller();
+                    else if (choice2 == "d") a2.DepositCaller();
+                    else Console.WriteLine("Invalid Choice! Please enter again");
+
+                }
+                while (choice2 != "w" && choice2 != "d");
+
+                round++;
+            }
+            if (a1.Balance > a2.Balance) Console.WriteLine($"\n{a1.Accountholder} Won with Balance: {a1.Balance}");
+            else Console.WriteLine($"\n{a2.Accountholder} Won with Balance: {a2.Balance}");
+        }
+    }
+}
